2520e398b1d4f1c370a3be549e70311c31478637
macurandb <macurandb@gmail.com>
2020-06-13 00:22:52 -0400

    ref: Reorganized security tests

---
+++ apps/security/tests/__init__.py
@@ -0,0 +1,3 @@
+from apps.security.tests.security.test_register_user import *
+from apps.security.tests.security.test_login import *
+
--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from .security.test_register_user import *
-from .security.test_login import *
